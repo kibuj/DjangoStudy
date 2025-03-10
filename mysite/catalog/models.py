7ab6c1d2f9ec8d22e1757eaa4974d11b6cd6f884
bogdan <bogdankibuj@gmail.com>
2025-03-10 16:45:33 +0200

    add models - author and own class language

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -97,5 +97,34 @@ class BookInstance(models.Model):
         ordering = ['due_back']
 
     def __str__(self):
-        """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+
+class Language(models.Model):
+    #own class (print by me)
+    language = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['language']
+
+    def get_absolute_url(self):
+        return reverse('language-write', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
