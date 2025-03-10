5dfb900fa3b690f405a4e989e6b863bfd366c90f
bogdan <bogdankibuj@gmail.com>
2025-03-10 16:31:56 +0200

    create Model - BookInstance

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
+import uuid # Required for unique book instances
 
 class MyModelName(models.Model):
 
@@ -20,7 +21,6 @@ class MyModelName(models.Model):
 
 
 class Genre(models.Model):
-    """Model representing a book genre."""
     name = models.CharField(
         max_length=200,
         unique=True,
@@ -28,7 +28,6 @@ class Genre(models.Model):
     )
 
     def __str__(self):
-        """String for representing the Model object."""
         return self.name
 
     def get_absolute_url(self):
@@ -45,7 +44,6 @@ class Genre(models.Model):
         ]
 
 class Book(models.Model):
-    """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
     # Foreign Key used because book can only have one author, but authors can have multiple books.
@@ -64,9 +62,40 @@ class Book(models.Model):
         Genre, help_text="Select a genre for this book")
 
     def __str__(self):
-        """String for representing the Model object."""
         return self.title
 
     def get_absolute_url(self):
-        """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+
+
+class BookInstance(models.Model):
+
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
