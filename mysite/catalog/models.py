efa5d54874dfba6e7c7aa2abe0dd72674c4e433e
bogdan <bogdankibuj@gmail.com>
2025-03-10 15:10:03 +0200

    add models for library

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class MyModelName(models.Model):
+
+    my_field_name = models.CharField(max_length=20, help_text='Enter field documentation')
+
+    # Metadata
+    class Meta:
+        ordering = ['-my_field_name']  # сортує записи в порядку спадання
+
+    # Methods
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)]) #  отримання URL-адреси конкретного об'єкта.
+
+    def __str__(self):
+        return self.my_field_name
